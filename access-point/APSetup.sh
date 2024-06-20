8f16ac33be33bdee74ab81fea23a4bb1904d8deb
TheExtraCrispy <ianrose30@yahoo.com>
2024-06-20 10:48:13 -0400

    Updated access point setup script

--- access-point/APSetup.sh
+++ access-point/APSetup.sh
@@ -9,7 +9,7 @@ sudo systemctl enable hostapd
 
 sudo apt install dnsmasq
 
-last=$(tail -n 1 test.txt)
+last=$(tail -n 1 /etc/dhcpcd.conf)
 
 if [ "$last" = "hook wpa_supplicant" ]
 then
@@ -17,24 +17,13 @@ then
 else
 	echo -e "Configuring dhcpcd.conf..."
 	echo -e "Saving original as dhcpcd.conf.orig"
-	#mv /etc/dhcpcd.conf /etc/dhcpcd.conf.orig
+	mv /etc/dhcpcd.conf /etc/dhcpcd.conf.orig
 
-	echo -e "\ninterface wlan0 \nhook wpa_supplicant" >> test.txt
+	echo -e "\ninterface wlan0 \nhook wpa_supplicant" >> /etc/dhcpcd.conf
 fi
 
 echo -e "dhcpcd.conf configuration done.\n"
 
-
-#echo -e "Configuring routed-ap.conf"
-
-#if [ -f /etc/sysctl.d/routed-ap.conf ]
-#then
-#	echo -e "routed-ap.conf already exists."
-#else
-#	echo -e "Creating routed-ap.conf"
-#	echo "net.ipv4.ip_forward=1"
-#fi
-#echo -e "routed-ap.conf done.\n"
 
 
 echo -e "Configuring dnsmasq.conf"
@@ -44,8 +33,8 @@ then
 	echo -e "dnsmasq.conf already configured."
 else
 	echo -e "Saving original as dnsmasq.conf.orig"
-	#mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
-	#cp dnsmasq.conf /etc/dnsmasq.conf
+	mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+	cp dnsmasq.conf /etc/dnsmasq.conf
 fi
 
 echo -e "dnsmasq.conf done."
@@ -57,16 +46,19 @@ if [[ $# -ge 2 ]]
 then
 	sed -i "s/^ssid=.*/ssid=$1/" hostapd.conf
 	sed -i "s/^wpa_passphrase=.*/wpa_passphrase=$2/" hostapd.conf
+
+	cp hostapd.conf /etc/hostapd/hostapd.conf
+
+	echo -e "hostapd configured.\n"
+
+	echo -e "Unblocking wifi in rfkill."
+
+	rfkill unblock wlan
+
+	echo -e "\n DONE!"
+
+	echo -e"\n Please make sure your network location is set correctly in raspi-config, then reboot the pi. Hopefully your network will come up."
+
 else
 	echo -e "Please provide your desired SSID and passphrase as 2 arguments."
 fi
-
-echo -e "hostapd configured.\n"
-
-echo -e "Unblocking wifi in rfkill."
-
-rfkill unblock wlan
-
-echo -e "\n DONE!"
-
-echo -e"\n Please make sure your network location is set correctly in raspi-config, then reboot the pi. Hopefully your network will come up."
