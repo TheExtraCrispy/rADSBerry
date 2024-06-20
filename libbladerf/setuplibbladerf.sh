f99f260adc6dee3d3af0c272977c8322af6462fe
TheExtraCrispy <ianrose30@yahoo.com>
2024-06-20 11:59:44 -0400

    added script to set coordinates and install libbladerf

---
+++ libbladerf/setuplibbladerf.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#clone repo
+
+cd libbladerf
+
+cd host
+
+mkdir build
+
+cd build
+
+#do build stuff
+#Exclude all but bladeRF
---
+++ setCoordinates.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+#TODO
+#Find and replace with sed the coordinates in dump1090-fa config
