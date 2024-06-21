49779c9aee50beeab8d00b110b18ed296b4569ab
TheExtraCrispy <ianrose30@yahoo.com>
2024-06-21 13:44:57 -0400

    Made script to set coordinates more easily

--- setCoordinates.sh
+++ setCoordinates.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-#TODO
-#Find and replace with sed the coordinates in dump1090-fa config
+
+if [[ $# -eq 2 ]]
+then
+	sed "s/^RECEIVER_LAT=.*/RECEIVER_LAT=$1/" etc/default/dump1090-fa
+	sed "s/^RECEIVER_LON=.*/RECEIVER_LON=$2/" etc/default/dump1090-fa
+
+	echo -e "New coordinates set as $1, $2"
+else
+	echo -e "Provide 2 arguments. LAT and LON. Ensure that these are decimal numbers only. Negative for S and W"
+fi
