bb394b69bae167e784981affa4d517107f2c3093
TheExtraCrispy <51653341+TheExtraCrispy@users.noreply.github.com>
2024-06-21 14:35:19 -0400

    fix missing 'fi'

--- tar1090/downloadOfflineMap.sh
+++ tar1090/downloadOfflineMap.sh
@@ -5,5 +5,5 @@ then
 	curl https://raw.githubusercontent.com/wiedehopf/adsb-scripts/master/osm_tiles_offline.sh | sudo bash
 else
 	curl https://raw.githubusercontent.com/wiedehopf/adsb-scripts/master/osm_tiles_offline_10.sh | sudo bash
-
+fi
 
