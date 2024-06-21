4a9c2087dbe36ab277a607edd7b3633245641339
TheExtraCrispy <ianrose30@yahoo.com>
2024-06-21 16:09:14 -0400

    Changing dump1090 setup to build from source and make a service

--- dump1090-fa/setupdump1090.sh
+++ dump1090-fa/setupdump1090.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
-apt install dump1090-fa
+git clone https://github.com/flightaware/dump1090
+
+cd dump1090
+
+make RTLSDR=no HACKRF=no LIMESDR=no SOAPYSDR=no
+
+cd ..
 
 cp dump1090-fa /etc/default/dump1090-fa
+
+mkdir /usr/share/dump1090-fa
+cp start-dump1090-fa /usr/share/dump1090-fa
+
+cp dump1090-fa.service /etc/systemd/system/dump1090-fa.service
+
+
+systemctl daemon-reload
+systemctl enable dump1090-fa.service
