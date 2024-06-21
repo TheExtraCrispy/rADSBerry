7789899505cbeef5d793345e442fc931110a16bb
TheExtraCrispy <ianrose30@yahoo.com>
2024-06-21 14:01:25 -0400

    Scripts to set up my bladerf-adsb repo and libbladerf

--- bladerf-adsb/bladerfsetup.sh
+++ bladerf-adsb/bladerfsetup.sh
@@ -8,3 +8,21 @@ systemctl daemon-reload
 systemctl enable bladeRF-adsb.service
 
 
+echo "Downloading bladerf-adsb..."
+git clone github.com/theextracrispy/bladerf-adsb
+
+cd bladerf-adsb/bladerf_adsb
+
+echo "Downloading FPGA images..."
+wget http://nuand.com/fpga/adsbx40.rbf
+wget http://nuand.com/fpga/adsbx115.rbf
+wget http://nuand.com/fpga/adsbxA4.rbf
+wget http://nuand.com/fpga/adsbxA5.rbf
+wget http://nuand.com/fpga/adsbxA9.rbf
+
+echo "Building bladerf-adsb..."
+make
+
+echo "Done."
+
+
--- libbladerf/setuplibbladerf.sh
+++ libbladerf/setuplibbladerf.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
+echo -e "Installingng dependencies.."
+apt-get install libusb-1.0-0-dev libusb-1.0-0 build-essential cmake libncurses5-dev libtecla1 libtecla-dev pkg-config git wget
+
 #clone repo
+echo -e "Downloading repo..."
+git clone https://github.com/Nuand/bladeRF.git ./bladeRF
 
-cd libbladerf
-
-cd host
-
+echo -e "Building libbladerf..."
+cd bladeRF/host
 mkdir build
-
 cd build
 
-#do build stuff
-#Exclude all but bladeRF
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local -DINSTALL_UDEV_RULES=ON ../
+sudo make
+sudo make install
+sudo ldconfig
+
+echo -e "Done. Hopefully worked."
