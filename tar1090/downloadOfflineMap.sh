ceb8a0207309a4add7474c17fb002cd61f2e496d
TheExtraCrispy <51653341+TheExtraCrispy@users.noreply.github.com>
2024-07-15 12:01:58 -0400

    fixed maps being backwards

--- tar1090/downloadOfflineMap.sh
+++ tar1090/downloadOfflineMap.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" = "extra" ]
 then
-	curl https://raw.githubusercontent.com/wiedehopf/adsb-scripts/master/osm_tiles_offline.sh | sudo bash
-else
 	curl https://raw.githubusercontent.com/wiedehopf/adsb-scripts/master/osm_tiles_offline_10.sh | sudo bash
+else
+  	curl https://raw.githubusercontent.com/wiedehopf/adsb-scripts/master/osm_tiles_offline.sh | sudo bash
 fi
 
