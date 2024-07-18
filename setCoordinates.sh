02927efd44869dc449627cb211f7037306196974
TheExtraCrispy <51653341+TheExtraCrispy@users.noreply.github.com>
2024-07-18 10:50:03 -0400

    Update setCoordinates.sh

--- setCoordinates.sh
+++ setCoordinates.sh
@@ -3,8 +3,8 @@
 
 if [[ $# -eq 2 ]]
 then
-	sed "s/^RECEIVER_LAT=.*/RECEIVER_LAT=$1/" etc/default/dump1090-fa
-	sed "s/^RECEIVER_LON=.*/RECEIVER_LON=$2/" etc/default/dump1090-fa
+	sed -i "s/^RECEIVER_LAT=.*/RECEIVER_LAT=$1/" /etc/default/dump1090-fa
+	sed -i "s/^RECEIVER_LON=.*/RECEIVER_LON=$2/" /etc/default/dump1090-fa
 
 	echo -e "New coordinates set as $1, $2"
 else
